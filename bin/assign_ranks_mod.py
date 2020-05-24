ee1966499d02606c90b354f1084700405725c05c
Edgar Gilchrist <edgar.gilchrist@oracle.com>
2020-05-24 07:01:50 -0400

    A new module for ranking the overall results

---
+++ bin/assign_ranks_mod.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import re
+import sys
+
+
+def sortFn(line):
+    return float(line.split('|')[4])
+
+
+def assign_ranks(datafile,outputfile):
+    outfile = open (outputfile, "w+")
+    with open(datafile) as fp: 
+        all_lines = fp.readlines()
+        newlist = []
+        counter=0
+
+        all_lines.sort(key=sortFn,reverse=True)
+
+        for line in all_lines:
+            line=line[:-1] # chomp 
+            counter+=1
+            outfile.write("{}|{}\n".format(counter, line))
+    outfile.close()
+
