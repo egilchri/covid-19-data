f551ea5f2af0817b352bd75c0298d23f8796c1c1
Edgar Gilchrist <edgar.gilchrist@oracle.com>
2020-11-21 07:29:37 -0500

    use_properties Wrap everything in a try/except to try to see where backend is dying

--- bin/county_driver_more_better.py
+++ bin/county_driver_more_better.py
@@ -54,70 +54,81 @@ filename = "output/{}.{}.{}.txt".format(base_file, today_date, what_to_trace)
 filename_sorted = "output/{}.{}.{}.sorted".format(base_file, today_date, what_to_trace)
 outfile = open (filename, "w+")
 
-with open(counties_file) as fp: 
-    all_lines = fp.readlines()
+
+try:
+  with open(counties_file) as fp: 
+      all_lines = fp.readlines()
 
 
-for line in all_lines:
-    line = line.rstrip()  
-    [state,countyAndFips] = line.split('.')
-    [county,fips] = countyAndFips.split('|')
-    state = state.replace("_", " ")
-    county = county.replace("_", " ")
-    # print "state: %s county: %s" % (state, county)
-    try:
-        process_my_counties(state=state, county=county, fips=fips, mathOperation='trendline', whatToTrack=what_to_trace, outfile=outfile, popdict=popdict);
-    except Exception as e:
-        # Just print(e) is cleaner and more likely what you want,
-        # but if you insist on printing message specifically whenever possible...
-        logging.exception("An exception was thrown!")
-#        if hasattr(e, 'message'):
-#            print(e.message)
-#        else:
-#            print(e)
+  for line in all_lines:
+      line = line.rstrip()  
+      [state,countyAndFips] = line.split('.')
+      [county,fips] = countyAndFips.split('|')
+      state = state.replace("_", " ")
+      county = county.replace("_", " ")
+      # print "state: %s county: %s" % (state, county)
+      try:
+          process_my_counties(state=state, county=county, fips=fips, mathOperation='trendline', whatToTrack=what_to_trace, outfile=outfile, popdict=popdict);
+      except Exception as e:
+          # Just print(e) is cleaner and more likely what you want,
+          # but if you insist on printing message specifically whenever possible...
+          logging.exception("An exception was thrown!")
+  #        if hasattr(e, 'message'):
+  #            print(e.message)
+  #        else:
+  #            print(e)
 
-outfile.close()
-assign_ranks (datafile=filename, outputfile=filename_sorted)
-
-
-jsonfile_name = "{}.json".format(filename_sorted)
-csvfile = open(filename_sorted, 'r')
-jsonfile = open(jsonfile_name, 'w')
-
-fieldnames = ("order", "slope","state","county","code","rate","now", "wk_ago","rate_of_increase")
-reader = csv.DictReader(csvfile, fieldnames, delimiter='|')
-out = json.dumps( [ row for row in reader ], indent=4, separators=(',', ': ') )
-
-jsonfile.write(out)
-jsonfile.close()
-###
+  outfile.close()
+  assign_ranks (datafile=filename, outputfile=filename_sorted)
 
 
+  jsonfile_name = "{}.json".format(filename_sorted)
+  csvfile = open(filename_sorted, 'r')
+  jsonfile = open(jsonfile_name, 'w')
 
-# Create an S3 client
-s3 = boto3.client('s3')
-s3resource = boto3.resource('s3')
-filename = jsonfile_name
-# bucket_name = "covid-counties"
-bucket_name = "covid-counties"
+  fieldnames = ("order", "slope","state","county","code","rate","now", "wk_ago","rate_of_increase")
+  reader = csv.DictReader(csvfile, fieldnames, delimiter='|')
+  out = json.dumps( [ row for row in reader ], indent=4, separators=(',', ': ') )
 
-mimetype = 'application/json'
-# mimetype = 'text/json' # you can programmatically get mimetype using the `mimetypes` module
+  jsonfile.write(out)
+  jsonfile.close()
+  ###
+except Exception as e:
+  track = traceback.format_exc()
+  logging.exception(track)
+  print(track)
 
 
-print ("filename: {}".format (filename))
+try:
+
+  # Create an S3 client
+  s3 = boto3.client('s3')
+  s3resource = boto3.resource('s3')
+  filename = jsonfile_name
+  # bucket_name = "covid-counties"
+  bucket_name = "covid-counties"
+
+  mimetype = 'application/json'
+  # mimetype = 'text/json' # you can programmatically get mimetype using the `mimetypes` module
 
 
-DO_UPLOAD_TO_S3= os.environ['DO_S3_UPLOAD']
-if (DO_UPLOAD_TO_S3 == "yes"):
-    s3.upload_file(
-        Filename=filename,
-        Bucket=bucket_name,
-        Key=filename,
-        ExtraArgs={
-            'ACL': 'public-read',
-            "ContentType": mimetype
-        }
-    )
+  print ("filename: {}".format (filename))
 
+
+  DO_UPLOAD_TO_S3= os.environ['DO_S3_UPLOAD']
+  if (DO_UPLOAD_TO_S3 == "yes"):
+      s3.upload_file(
+          Filename=filename,
+          Bucket=bucket_name,
+          Key=filename,
+          ExtraArgs={
+              'ACL': 'public-read',
+              "ContentType": mimetype
+          }
+      )
+
+except Exception as e:
+  track = traceback.format_exc()
+  logging.exception(track)
+  print(track)
 
